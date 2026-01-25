4582f36862da212c24087d5e1d28f5eca75953e9
yogiramsuratkumarbhajans-netizen <yogiramsuratkumarbhajans@gmail.com>
2026-01-25 17:06:11 +0530

    Update firebase.js

--- yrsk-english/src/firebase.js
+++ yrsk-english/src/firebase.js
@@ -4,13 +4,13 @@ import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBypTSxsv-XWZ_YKhvfpn6qCCVv-gU6R0s",
+  apiKey: "",
   authDomain: "yrsk-divya-vani-english.firebaseapp.com",
   projectId: "yrsk-divya-vani-english",
   storageBucket: "yrsk-divya-vani-english.firebasestorage.app",
-  messagingSenderId: "94717252824",
-  appId: "1:94717252824:web:e30a8154f29ac72fbdf3ad",
-  measurementId: "G-8C7MT822FB"
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "G-"
 };
 
 // Initialize Firebase
